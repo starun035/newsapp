933cfb99ca5648428d0522d6496f30abc2510a83
starun035 <31860939+starun035@users.noreply.github.com>
2022-05-17 20:43:59 +0530

    Add files via upload

---
+++ NewsItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export class NewsItem extends React.Component {
+    // constructor() {
+    //     super();
+    // }
+    render() {
+        let {title, description, imageUrl, newsUrl} = this.props;
+        return (
+            <div className="card" style={{width: '18rem'}}>
+                <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href={newsUrl} className="btn btn-sm btn-primary">Read more</a>
+                </div>
+            </div>
+        );
+    }
+}
